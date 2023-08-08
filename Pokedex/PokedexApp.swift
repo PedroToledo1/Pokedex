6e65688220e233e86df2b74e725beed2f42d6026
PedroToledo1 <59672144+PedroToledo1@users.noreply.github.com>
2023-08-08 19:08:38 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,20 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Pedro Toledo on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
